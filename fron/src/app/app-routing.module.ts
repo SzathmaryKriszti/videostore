44fafd256e451d1f3165ca412cf0f05cdcd845fa
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-02-28 15:19:43 +0100

    -init flowbite -add html to videostore

--- fron/src/app/app-routing.module.ts
+++ fron/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {VideostoreComponent} from "./videostore/videostore.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: VideostoreComponent, pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
