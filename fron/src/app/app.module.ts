7469c2f6b2e97cb6d45f416ce1207153913ad2d4
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-02-28 14:48:05 +0100

    -add tailwind css to the project

--- fron/src/app/app.module.ts
+++ fron/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideostoreComponent } from './videostore/videostore.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideostoreComponent
   ],
   imports: [
     BrowserModule,
