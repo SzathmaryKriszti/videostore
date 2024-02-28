9e91bf6acacfe0408ba5129e336848a6048b814a
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-02-28 13:37:07 +0100

    Initial commit

---
+++ src/test/java/com/kriszti/app/videostore/VideostoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kriszti.app.videostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideostoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
